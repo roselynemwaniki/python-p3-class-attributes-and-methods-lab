24eaa7a100e5ac1f07892165c076431120acf355
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-12-03 14:27:35 +0300

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,30 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        
+        # Update genre count
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+        Song.genre_count[genre] += 1
+        
+        # Update artist count
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+        Song.artist_count[artist] += 1
+        
+        # Track unique genres and artists
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+          
